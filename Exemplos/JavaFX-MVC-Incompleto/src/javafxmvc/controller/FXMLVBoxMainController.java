9c8e932b3a48cb300daca79835d677b01027298f
Lucas Wantz da Motta <lucaswantz@hotmail.com>
2017-07-19 16:51:14 -0300

    Criação Main
    Criação da classe main do programa, e possibilidade de inicializar o programa.

--- Exemplos/JavaFX-MVC-Incompleto/src/javafxmvc/controller/FXMLVBoxMainController.java
+++ Exemplos/JavaFX-MVC-Incompleto/src/javafxmvc/controller/FXMLVBoxMainController.java
@@ -7,18 +7,28 @@ package javafxmvc.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
 
-/**
- * FXML Controller class
- *
- * @author lucas.motta
- */
 public class FXMLVBoxMainController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @ FXML
+    private MenuItem menuItemCadastrosCategorias;
+    @ FXML
+    private MenuItem menuItemCadastrosClientes;
+    @ FXML
+    private MenuItem menuItemCadastrosProdutos;
+    @ FXML
+    private MenuItem menuItemProcessosVendas;    
+    @ FXML
+    private MenuItem menuItemGraficosVendasPorMes;
+    @ FXML
+    private MenuItem menuItemRelatoriosQuantidadeProdutosEstoque;
+    @ FXML
+    private AnchorPane anchorPane;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
