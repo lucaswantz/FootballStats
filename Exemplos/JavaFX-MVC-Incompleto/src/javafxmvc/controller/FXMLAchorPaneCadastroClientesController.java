474be98d7bb66a9abbfe6282dd2a492d276f0ed0
Lucas Wantz da Motta <lucaswantz@hotmail.com>
2017-07-20 12:51:20 -0300

    Tela de clientes
    Criada a tela de cadastro dos clientes, ainda em desenvolvimento.

---
+++ Exemplos/JavaFX-MVC-Incompleto/src/javafxmvc/controller/FXMLAchorPaneCadastroClientesController.java
@@ -0,0 +1,54 @@
+package javafxmvc.controller;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.util.List;
+import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafxmvc.model.dao.ClienteDAO;
+import javafxmvc.model.database.Database;
+import javafxmvc.model.database.DatabaseFactory;
+import javafxmvc.model.domain.Cliente;
+
+public class FXMLAchorPaneCadastroClientesController implements Initializable {
+
+    @FXML
+    private TableView<Cliente> tableViewClientes;
+    @FXML
+    private TableColumn<Cliente, String> tableColumnClienteNome;
+    @FXML
+    private TableColumn<Cliente, String> tableColumnClienteCPF;
+    @FXML
+    private Button buttonInserir;
+    @FXML
+    private Button buttonAlterar;
+    @FXML
+    private Button buttonRemover;
+    @FXML
+    private Label labelClienteCodigo;
+    @FXML
+    private Label labelClienteNome;
+    @FXML
+    private Label labelClienteCPF;
+    @FXML
+    private Label labelClienteTelefone;
+
+    private List<Cliente> listClientes;
+    private ObservableList<Cliente> observableListClientes;
+    
+    private final Database database = DatabaseFactory.getDatabase("postgresql");
+    private final Connection connection = database.conectar();
+    private final ClienteDAO clienteDAO = new ClienteDAO();
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
